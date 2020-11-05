08d848f663685fa396a9320ed00ff717b5b3b787
Ramiro Méndez López <ramirog8k@gmail.com>
2020-11-05 12:19:23 -0600

    Structure of NN and operations in layers

--- main.py
+++ main.py
@@ -1,24 +1,57 @@
+'''
+Algorithm: Neural Network with dynamic activation functions
+Date: Thursday November 5th, 2020
+Autor: Ramiro Mendez, based in Carlos Santana Model
+'''
 import numpy as np
 import scipy as sc
 import matplotlib.pyplot as plt
-# Dataset
-from sklearn.datasets import make_circles
+from sklearn.datasets import make_circles   # Dataset
 
 # Neural layers
 from neural_layer import Neural_Layer
 
 '''
-Anonymous Sigmoid activation function and derivate
-index 0 actual function
-index 1 derivation function
+Anonymous function (commented) 
+    Sigmoid activation function and derivate
+    index 0 actual function
+    index 1 derivation function
 '''
 # sigm = (lambda x: 1 / (1 + np.e ** (-x)),
 #         lambda x: x * (1 - x))
+
+
 def sigm(x, derivate=False):
-    u =  1 / (1 + np.e ** (-x))
+    u = 1 / (1 + np.e ** (-x))
     du = x * (1 - x)
-    return du if (derivate==True) else u
-    
+    return du if (derivate == True) else u
+
+
+'''
+Cost Function (anonymous commented) 
+    Least Mean Square function and derivate
+    Returns difference between output and desired 
+'''
+# lms_cost = (lambda output, desired: np.mean((output - desired) ** 2),
+#             lambda output, desired: (output - desired))
+
+
+def lms_cost(output, desired, derivate=False):
+    u = np.mean((output - desired) ** 2)
+    du = np.mean(output - desired)
+    return du if (derivate == True) else u
+
+
+'''
+Neural Network topology
+    Returns nn  Neural network array with layers inside
+    topology    Vector to instantiate n neurons per layer in NN
+    act_f       Activation function
+
+    For item in array 'topology' creates actual layer 
+    Instantiates 'n' inputs to the next one and 'act_f' per layer
+'''
+
 
 def create_nn(topology, act_f):
     nn = []
@@ -26,12 +59,28 @@ def create_nn(topology, act_f):
         nn.append(Neural_Layer(connections, topology[i+1], act_f))
     return nn
 
+
+def train(neural_network, samples, desired, lms_cost, lr=0.5):
+    '''
+    Forward pass -> 
+        Gets samples and desired, passes through layers
+        Applies weighted sum (samples * weights + bias)
+        Weighted sum to Activation function
+    '''
+    output = [(None, samples)]
+    for _, layer in enumerate(neural_network):
+        # @ it is used to matrix multiplication
+        z = output[-1][1] @ layer.weights + layer.bias
+        a = layer.activation_f(z)
+        output.append((z, a))
+
+    print(output[-1][1])
+
+
 if __name__ == "__main__":
-    '''
-    Create dataset
-    n   stands for number of samples to train with
-    p   number of inputs per sample, characteristics of every sample
-    '''
+    # Create dataset
+    # n   stands for number of samples to train with
+    # p   number of inputs per sample, characteristics of every sample
     n = 500
     p = 2
 
@@ -55,7 +104,7 @@ if __name__ == "__main__":
     # Samples in first class, in this case class  '0'
     plt.scatter(samples[desired == 0, 0],
                 samples[desired == 0, 1], color="green")
-    
+
     # Samples in first class, in this case class  '1'
     plt.scatter(samples[desired == 1, 0],
                 samples[desired == 1, 1], color="red")
@@ -64,7 +113,9 @@ if __name__ == "__main__":
     Final is 1 because classification is binary, based in 0 or 1
     '''
     topology = [p, 4, 8, 16, 8, 4, 1]
-    print(create_nn(topology, sigm))
+    neural_network = create_nn(topology, sigm)
+
+    train(neural_network, samples, desired, lms_cost)
 
     # plt.axis("equal")
     # plt.show()
